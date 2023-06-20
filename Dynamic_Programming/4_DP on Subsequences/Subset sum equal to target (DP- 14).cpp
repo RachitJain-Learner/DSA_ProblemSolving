37c94a64c77b36ca4764e7262334639fa77b6e51
Rachit <rjrachit2468@gmail.com>
2023-06-20 12:12:41 +0530

    Added Memoization on Subset Sum

--- Dynamic_Programming/4_DP on Subsequences/Subset sum equal to target (DP- 14).cpp
+++ Dynamic_Programming/4_DP on Subsequences/Subset sum equal to target (DP- 14).cpp
@@ -11,27 +11,77 @@ using namespace std;
 // If infinite supply then use "for loop" to take repetitive cases
 
 class Solution{  
-    
 public:
 
-    bool solve(vector<int>& arr, int sum, int ind){
-        if(ind == arr.size()){
-            return 0;
-        }
-        
+    
+
+    // Memoization Method-1 : from 0 to n
+    bool solveMemo(vector<int>& arr, int sum, int ind, vector<vector<int>>& dp){
         if(sum == 0)
             return true ;
+            
+        if(ind == arr.size())
+            return 0;
+
+        if(dp[ind][sum] != -1){
+            return dp[ind][sum];
+        }
+        
+        bool include = 0;
+        if(sum >= arr[ind]){
+            include = solveMemo(arr, sum-arr[ind], ind+1, dp);
+        }
+        bool exclude = solveMemo(arr, sum, ind+1, dp);
+        
+        return dp[ind][sum] = include || exclude ;
+    }
+    bool isSubsetSum(vector<int>arr, int sum){
+        
+        vector<vector<int>>dp(n, vector<int>(sum+1, -1)) ;
+        return solveMemo(arr, sum, 0, dp);
+    }
+    
+    // Memoization Method-2 : from n-1 to 0
+    bool solveMemo(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+        if(target==0)
+        return true;
+    
+        if(ind == 0)
+            return arr[0] == target;
+        
+        if(dp[ind][target]!=-1)
+            return dp[ind][target];
+            
+        bool notTaken = solveMemo(ind-1,target,arr,dp);
+        
+        bool taken = false;
+        if(arr[ind]<=target)
+            taken = solveMemo(ind-1,target-arr[ind],arr,dp);
+            
+        return dp[ind][target]= notTaken||taken;
+    }
+    bool isSubsetSum(vector<int>arr, int sum){
+
+        vector<vector<int>>dp(n, vector<int>(sum+1, -1)) ;
+        return solveMemo(n-1, sum ,arr,dp);
+    }
+
+    // Recursion
+    bool solve(vector<int>& arr, int sum, int ind){
+        if(sum == 0)
+            return true ;
+        
+        if(ind == arr.size())   
+            return 0;
         
         int include = 0;
         if(sum >= arr[ind]){
             include = solve(arr, sum-arr[ind], ind+1);
         }
-        
-        int exclude = 0 + solve(arr, sum, ind+1);
+        int exclude = solve(arr, sum, ind+1);
         
         return include || exclude ;
     }
-
     bool isSubsetSum(vector<int>arr, int sum){
         return solve(arr, sum, 0);
     }
